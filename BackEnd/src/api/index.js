1f4539e0ff90c62ac889371dc5b14f2737960d81
yashp2018 <yash03465@gmail.com>
2025-11-16 22:16:19 +0530

    Add files via upload

---
+++ BackEnd/src/api/index.js
@@ -0,0 +1,6 @@
+// BackEnd/api/index.js
+import app from '../src/server.js';
+// This file simply imports the Express app instance created in server.js
+// Vercel will use this as the entry point for your serverless function.
+
+export default app;
