2adc5efb1c8161009a9b61588efc8678b31f9c43
yashp2018 <yash03465@gmail.com>
2025-11-26 00:46:25 +0530

    Update API proxy target in vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://72.61.227.66:5000/',
         changeOrigin: true,
         secure: false,
       },
